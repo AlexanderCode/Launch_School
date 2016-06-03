1ecbe8fb9946a524189162001158fb871674b720
Alexander Ross <alexandersmithross@gmail.com>
2016-06-03 14:18:55 -0700

    lesson 2 first rework of the Calculator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -3,29 +3,117 @@
 # perform the operation on the two numbers
 # output the result
 
-puts "Hello and welcome to Alex's calculator!"
-puts ""
-puts "what is your first number?"
-number1 = gets.chomp
-puts ""
-puts number1 + " is your first number."
-puts ""
-puts "what is your second number?"
-number2 = gets.chomp
-puts ""
-puts number2 + " is your second number."
-puts ""
-puts "what is your operation? 1) add 2) subtract 3) multiply 4) divide"
-operator = gets.chomp
-
-if operator == '1'
-  result = number1.to_i + number2.to_i
-elsif operator == '2'
-  result = number1.to_i - number2.to_i
-elsif operator == '3'
-  result = number1.to_i * number2.to_i
-else 
-  result = number1.to_f / number2.to_f
+def prompt(message)
+  puts "=> #{message}"
 end
 
-puts "your answer is #{result}!"
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when '1'
+    'Adding'
+  when '2'
+    'Subtracting'
+  when '3'
+    'Multiplying'
+  when '4'
+    'Dividing'
+  end
+end
+
+
+number1 = ''
+number2 = ''
+
+prompt "Hello and welcome to Alex's calculator!"
+puts ''
+prompt "What is your name?"
+name = ''
+
+loop do 
+  name = gets.chomp
+    if name.empty?
+      prompt "I need a name!"
+    else
+      break
+    end
+  end
+
+prompt "Hi #{name}!"
+
+loop do #main loop
+
+  loop do
+  prompt "what is your first number?"
+  number1 = gets.chomp
+
+    if valid_number?(number1)
+      break
+    else
+      prompt "That does not look like a valid number to me..."
+    end
+  end
+
+  prompt number1 + " is your first number."
+  puts ''
+
+  loop do
+
+  prompt "what is your second number?"
+  number2 = gets.chomp
+
+    if valid_number?(number2)
+      break
+    else
+      prompt "That does not look like a valid number to me..."
+    end
+  end
+
+  prompt number2 + " is your second number."
+  puts ''
+  
+operator_prompt = <<-MSG
+  What operation would you like to preform?
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
+MSG
+
+
+  prompt operator_prompt
+  
+  operator = ''
+  loop do
+    operator = gets.chomp
+
+    if %w(1 2 3 4).include?(operator)
+      break
+    else
+      prompt "Must choose 1, 2, 3, or 4"
+    end
+  end
+
+prompt "#{operation_to_message(operator)} the two numbers..."
+
+  result = case operator 
+    when '1'
+    result = number1.to_i + number2.to_i
+    when '2'
+    result = number1.to_i - number2.to_i
+    when '3'
+    result = number1.to_i * number2.to_i
+    when '4'
+    result = number1.to_f / number2.to_f
+  end
+
+  prompt "your answer is #{result}!"
+  prompt "do you want to perform another calculation? (Y to calculate again)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with? 'y'
+end
+
+prompt "Thank you for using Alex's Calculator!"
